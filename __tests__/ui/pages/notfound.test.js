8bd33389524d5e9560f02dfb04fab1d4beed340a
luis eduardo canedo torres <luillys83@yahoo.es>
2022-07-30 07:43:27 -0500

    Se corrige el test NotFoutdPage el cual no tenia la ruta del archivo

--- __tests__/ui/pages/notfound.test.js
+++ __tests__/ui/pages/notfound.test.js
@@ -1,11 +1,11 @@
 import {render, screen} from "@testing-library/react";
-import NotFoundPage from "@/pages/[notFound]/index";
+import NotFoundPage from "@/pages/404/index";
 
 test("Should display the 404 Not found Page correctly", ()=>{
   render(<NotFoundPage/>);
 
   const goHomeBtn = screen.getByRole("button", {
-    name: /go home/i
+    name: /Go Home/
   });
 
   expect(goHomeBtn).toBeInTheDocument();
--- components/AppointmentForm/ContactItem.js
+++ components/AppointmentForm/ContactItem.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const ContactItem = (props) => {
+const ContactItem = ({ children }) => {
   return (
     <li className="flex gap-5 md:flex-col md:items-start lg:flex-row items-center mb-3 text-lg">
-      {props.children}
+      {children}
     </li>
   );
 };
