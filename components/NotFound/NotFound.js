c387839a112c81f8e5e2d4756528454e0fca590f
nicolaspc-dev <nicolaspc.wd@gmail.com>
2022-08-10 09:47:57 -0500

    404 page responsive feature added

--- components/NotFound/NotFound.js
+++ components/NotFound/NotFound.js
@@ -1,27 +1,21 @@
 import React from "react";
-import Image from "next/image";
-import Grumpy404 from "../../public/assets/images/Grumpy404.png";
 import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="col-start-1 col-end-3 md:col-start-2 md:col-end-8 flex justify-center items-center row-span-2 w-full relative ">
-      <div className="md:hidden">
-        <Image src={Grumpy404} layout="fill" objectFit="cover" />
-      </div>
-
-      <div className="md:col-start-5 md:col-end-8 flex flex-col justify-between items-center h-[90%] mt-10 z-10 md:z-0">
+    <div className="w-full h-[100vh] flex items-center justify-center font-inter">
+      <div className="h-[80%] flex flex-col justify-around">
         <div>
           <p className="text-[7em] md:text-[15rem] text-center text-[#565656] font-semibold">
             404
           </p>
         </div>
-        <div className="flex flex-col justify-center items-center gap-2">
+        <div className="flex flex-col justify-center items-center gap-10">
           <h1 className="text-3xl  md:text-4xl text-center font-bold text-[#565656]">
             Uh oh we have a problem{" "}
           </h1>
           <Link href="/">
-            <button className="bg-[#154D4D] text-white py-3 px-6 rounded-md tracking-widest hover:bg-[#217a7a] w-[16rem] ">
+            <button className="bg-[#154D4D] text-white py-3 px-6 rounded-md tracking-widest hover:bg-[#217a7a]  w-[12rem] sm:w-[16rem] sm:py-4 text-sm sm:text-md">
               GO HOME
             </button>
           </Link>
